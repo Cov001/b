ef3f6bff615492ca1459615c0ea6a401227da5c6
Cov001 <155533946+Cov001@users.noreply.github.com>
2024-10-12 23:40:41 +0700

    Update Meet.jsx

--- src/pages/Meet.jsx
+++ src/pages/Meet.jsx
@@ -163,7 +163,7 @@ const Meet = () => {
 
     const url =
       process.env.NODE_ENV === "production"
-        ? "https://video-conferencing-webapp.vercel.app/"
+        ? "https://b-ochre-two.vercel.app/"
         : "http://localhost:3000";
 
     window.location.href = url;
